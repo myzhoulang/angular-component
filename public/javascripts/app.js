0431a26dc5d2ac68aa0ffbe8ffa298e3b0ef5599
myzhoulang <604389771@qq.com>
2016-01-18 15:31:23 +0800

    add sug

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,300 @@
-(function(angular, $){
-	'use strict';
-	var app = angular.module('app', []);
+(function (angular, $) {
+    'use strict';
+
+    var app = angular.module('app', []);
+    var datas = [
+        {
+            name: '自营商品',
+            id: 1
+        },
+        {
+            name: '测试供应商',
+            id: 2
+        },
+        {
+            name: '杭州尚达汽配有限公司',
+            id: 3
+        },
+        {
+            name: '杭州万品汽配城联沃汽车配件商行',
+            id: 4
+        },
+        {
+            name: '宁波市海曙顺风汽配商行',
+            id: 5
+        },
+        {
+            name: '宁波市海曙顺风汽配商行',
+            id: 6
+        },
+        {
+            name: '杭州东楠贸易有限公司',
+            id: 7
+        },
+        {
+            name: '宁波博德联合汽配贸易有限公司',
+            id: 8
+        },
+        {
+            name: '杭州申令贸易有限公司',
+            id: 9
+        },
+        {
+            name: '浙江景厚贸易有限公司',
+            id: 10
+        },
+        {
+            name: '宁波北仑宁虹机电有限公司',
+            id: 11
+        },
+        {
+            name: '杭州轮速汽配有限公司',
+            id: 12
+        },
+        {
+            name: '广东爱车小屋实业发展股份有限公司',
+            id: 13
+        },
+ ];
+    var sugTlp = '<div class="sug">' +
+        '<div class="sug-input">' +
+        '<input type="search" ng-model="sugSelect.name" placeholder="输入关键字搜索" />' +
+        '</div>' +
+        '<div class="sug-lists" ng-show="suglists.length > 0">' +
+        '<ul>' +
+        '<li ng-repeat="item in suglists" ng-class="{active: iNow === $index}">{{ item.name }}</li>' +
+        '</ul></div></div>';
+
+    app.directive('sug', ['$filter', '$timeout', function ($filter, $timeout) {
+        return {
+            restrict: 'AE',
+            template: sugTlp,
+            scope: {
+                source: '=',
+                placeholder: '@',
+                selectItem: '=',
+                sugSelect: '=',
+                onSearch: '&',
+                onSelect: '&',
+                onBlur: '&',
+                item: '='
+            },
+            controller: function ($scope, $element) {
+                var $sug = $($element);
+                var $input = $sug.find('input[type="search"]');
+                var $sugList = $sug.find('.sug-lists');
+                var $ul = $sugList.find('ul');
+                var $aLi = $sugList.find('li');
+                console.log($scope.sugSelect)
+                    // $scope.searchTxt = $scope.sugSelect.name;
+
+                //                $scope.$watch('sugSelect', function (oldVal, newVal) {
+                //                    //debugger
+                //                    if (angular.isObject(newVal)) {
+                //                        if (angular.isString(newVal.name)) {
+                //                            $scope.searchTxt = newVal.name
+                //                        }
+                //                    }
+                //                });
+
+                if (angular.isArray($scope.source)) {
+                    $scope.suglists = $scope.sugSelectsource;
+                }
+
+                angular.extend($scope, {
+
+                    open: function () {
+                        $sugList.show();
+                    },
+
+                    close: function () {
+                        $timeout(function () {
+                            $sugList.hide();
+                        }, 10)
+                    },
+
+                    select: function (select) {
+                        $scope.onSelect({
+                            select: select
+                        });
+                    },
+
+                    blur: function (keyword) {
+                        $scope.onBlur({
+                            search: {
+                                selectItem: $scope.selectItem,
+                                keyword: keyword
+                            }
+                        });
+                    },
+
+                    search: function (keyword) {
+                        $scope.iNow = 0;
+                        if (angular.isArray($scope.source)) {
+                            return $filter('filter')($scope.source, {
+                                name: keyword
+                            });
+                        } else {
+                            if (angular.isFunction($scope.onSearch)) {
+                                $scope.onSearch()
+                            }
+                        }
+                    }
+                })
+            },
+            link: function (scope, element, attrs) {
+                var $sug = $(element);
+                var $input = $sug.find('input[type="search"]');
+                var $sugList = $sug.find('.sug-lists');
+                var $ul = $sugList.find('ul');
+                var $aLi = $sugList.find('li');
+                var $aLi2 = $aLi;
+                var timer = null;
+                var mouseoverTimer = null;
+                console.log(scope.sugSelect)
+                scope.iNow = 0;
+
+                $input.on({
+                    blur: function () {
+                        var $this = $(this);
+                        scope.close();
+                        scope.blur($this.val());
+                    },
+                    input: function (e) {
+                        $timeout.cancel(timer);
+                        var $this = $(this);
+                        timer = $timeout(function () {
+                            scope.suglists = scope.search($this.val());
+                            scope.iNow = 0;
+                            scope.open();
+                        }, 500);
+                    },
+                    keydown: function (e) {
+                        $aLi = $sugList.find('li');
+                        var keyCode = e.keyCode;
+                        switch (keyCode) {
+                        case 38:
+                            move(-1);
+                            break;
+
+                        case 40:
+                            move(1);
+                            break;
+
+                        case 13:
+                            var $item = $aLi.eq(scope.iNow);
+                            if ($item.length > 0) {
+                                $input.val($aLi.eq(scope.iNow).text());
+                                scope.close();
+                                scope.select({
+                                    selectItem: scope.selectItem,
+                                    sugSelect: scope.suglists[scope.iNow]
+                                });
+                                scope.sugSelect = scope.suglists[scope.iNow];
+                            } else {
+                                $input.blur();
+                            }
+
+                            break;
+
+                        default:
+                            break;
+                        }
+                    }
+                });
+
+                $sug.delegate('li', 'click', function () {
+                    $aLi = $sugList.find('li');
+                    scope.iNow = $(this).index();
+                    var itemData = scope.suglists[scope.iNow];
+                    scope.select({
+                        selectItem: scope.selectItem,
+                        sugSelect: itemData
+                    });
+                    scope.sugSelect = itemData;
+                    $input.val(itemData.name);
+                    scope.close();
+                }).delegate('li', 'mouseover', function () {
+                    $timeout.cancel(mouseoverTimer);
+                    var $this = $(this);
+                    mouseoverTimer = $timeout(function () {
+                        $aLi = $sugList.find('li');
+                        var index = $this.index();
+                        scope.iNow = index;
+                    }, 30)
+                });
+
+                function move(dir) {
+                    if (dir === 1) {
+                        if (scope.iNow === $aLi.length - 1) {
+                            scope.iNow = 0;
+                        } else {
+                            scope.iNow++;
+                        }
+                    } else if (dir === -1) {
+                        if (scope.iNow === 0) {
+                            scope.iNow = $aLi.length - 1
+                        } else {
+                            scope.iNow--;
+                        }
+
+                    }
+                    scope.$apply();
+                }
+            },
+        }
+    }]);
+
+    //controller
+    app.controller('testCtrl', ['$scope', function ($scope) {
+
+        var data = [{
+            supplier: {
+                name: '杭州尚达汽配有限公司',
+                userId: 3
+            },
+            addr: {
+                name: '',
+                addrId: ''
+            },
+            addrs: [],
+            num: 0
+        }]
+
+        angular.extend($scope, {
+            suglists: datas,
+            selected: function (select) {
+                console.log(select)
+                var selectItem = select.selectItem;
+                var sugSelect = select.sugSelect;
+
+                if (angular.isObject(sugSelect) && angular.isNumber(sugSelect.id)) {
+                    selectItem.addrs = [{
+                        name: '地址1',
+                        addrId: 1
+                    }, {
+                        name: '地址2',
+                        addrId: 2
+                    }, {
+                        name: '地址3',
+                        addrId: 3
+                    }];
+                    selectItem.addr = selectItem.addrs[0];
+                }
+            },
+            InuputBlur: function (seach) {
+                var selectItem = seach.selectItem;
+                if (!angular.isNumber(selectItem.id)) {
+                    console.log('根据关键字查询id');
+                }
+                console.log(seach);
+            },
+            suppliers: data,
+
+            addSuperlier: function () {
+
+            }
+        })
+    }]);
 
 })(angular, jQuery);
